0d771db132c5f85dbc668f9843a36e1ee1d839ea
tonyliaccount@github.com <tony.li.email@gmail.com>
2022-04-25 21:05:42 -0400

    Integrated gallery and image URLs.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let culture;
 let allCultures = [];
 let allImageUrl = [];
 let dropdownCulture;
+let gallery;
 
 /**
  * When called, will request for a list of objects from the server.
@@ -12,26 +13,45 @@ let dropdownCulture;
 
     axios.get('https://api.harvardartmuseums.org/object', 
     {
+        // First, we're going to just get the total number of items avaliable
         // Note: "classification": 26 is "Painting"
         params: {
             "apikey": '6205a0f6-7380-42b6-9a01-272d7c020f4f',
             "culture": culture,
             "hasimage": 1
         }
+    }).then( response => {
+        const totalRecords = response.data.info.totalrecords;
+
+        const size = (totalRecords > 200) ? 200 : totalRecords;
+
+        return axios.get('https://api.harvardartmuseums.org/object', 
+        {
+            params: {
+                "apikey": '6205a0f6-7380-42b6-9a01-272d7c020f4f',
+                "size": size,
+                "culture": culture,
+                "hasimage": 1,
+                "sort": "name"
+            }
+        });
+
     }).then(response => {
+
+        // Clear all stored image URLs
+        allImageUrl = [];
+
         const foundGalleryObjects = response.data;
-
-        console.log(foundGalleryObjects);
-
         foundGalleryObjects.records.forEach( record => {
 
             if (record.primaryimageurl) {
                 allImageUrl.push(record.primaryimageurl);
             }
         });
+    }).then( () => {
 
-        console.log(allImageUrl);
-
+        // Now that we have the URLs for the gallery, render them.
+        gallery.render(allImageUrl);
     }).catch(error => {
         console.error ("Failed to receive objects from API.");
         console.error (error);
@@ -40,40 +60,9 @@ let dropdownCulture;
 
 document.getElementById("getArt").addEventListener("click", requestGalleryObjects);
 
-dropdownCulture = new DropdownCulture(document.getElementById("culture-select"));
-
 // Web components
-
-class GalleryCard {
-    constructor() {
-
-    }
-    render (){
-        return `
-        <section class="gallery">
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-        </section>
-        `
-    }
-}
-
-class Gallery {
-    constructor() {
-        this.galleryCard = new GalleryCard;
-    }
-
-    render () {
-        return `
-            ${this.galleryCard.render()}
-        `
-    }
-}
-
-let galleryField = document.getElementById('gallery');
-let gallery = new Gallery;
-galleryField.innerHTML = gallery.render();
+dropdownCulture = new DropdownCulture(document.getElementById("culture-select"));
+gallery = new Gallery(document.getElementById('gallery'));
 
 // render images on the page
 const cardItem = document.querySelector('card');
