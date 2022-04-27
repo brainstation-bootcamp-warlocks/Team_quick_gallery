abddb7245dc672dd66ae9626bfaf13a4550c6e0a
tonyliaccount@github.com <tony.li.email@gmail.com>
2022-04-26 21:56:32 -0400

    Prelim changes -> render not working yet

--- scripts/components/Gallery.js
+++ scripts/components/Gallery.js
@@ -1,10 +1,41 @@
 class GalleryCard {
-    constructor(imageUrl) {
+    constructor(objectID) {
+        this.objectID = objectID;
+    }
+
+    requestData = (resolve, reject) => {
+        axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${this.objectID}`, 
+        {
+            // No additional parameters
+        }).then( response => {
+            // Keep track of the data
+            this.objectData = response.data;
+
+            // Isolate some specific fields we need
+            this.title = objectData.title;
+            this.objectDate = objectData.objectDate;
+            this.artistDisplayName = objectData.artistDisplayName;
+            this.primaryImageSmall = objectData.primaryImageSmall;
+            this.objectURL = objectData.objectURL;
+
+            resolve();
+
+        }).catch(error => {
+            console.error ("Failed to receive object from API.");
+            console.error (error);
+
+            reject();
+        });
+
         this.imageUrl = imageUrl;
     }
 
     render = () => {
-        return `<img class="card" src="${this.imageUrl}?width=160">`
+
+        let requestDataPromise = new Promise(this.requestData);
+        requestDataPromise.then({
+            return `<img class="card" src="${this.primaryImageSmall}?width=160">`;
+        });
     }
 }
 
@@ -13,21 +44,37 @@ class Gallery {
         this.parentContainerDOM = parentContainerDOM;
     }
 
-    generateCards = (listOfImageUrls) => {
-        this.cards = [];
+    populate = (listOfObjectIDs) => {
+        this.listOfObjectIDs = listOfObjectIDs;
 
-        listOfImageUrls.forEach(url => {
-            this.cards.push( (new GalleryCard(url)).render() );
-            }
-        );
+        this.galleryCards = [];
 
-        return this.cards.join('');
+        // Limit the number of cards to 5, or whichever is smaller
+        const smallerValue = (this.listOfObjectIDs.length > 5) ? 5 : this.listOfObjectIDs.length;
+
+        for (let index = 0; index < smallerValue; index ++) {
+            this.galleryCards.push( (new GalleryCard(this.listOfObjectIDs[index])) );
+        }
     }
 
-    render = (listOfImageUrls) => {
+    /**
+     * We're going to render some HTML based on what cards we have.
+     * @returns HTML
+     */
+     renderCards = () => {
+        let cardHTMLRender = [];
+
+        this.galleryCards.forEach(galleryCard => {
+            cardHTMLRender.push(galleryCard.render());
+        });
+
+        return cardHTMLRender.join('');
+    }
+
+    render = () => {
         this.parentContainerDOM.innerHTML = `
         <div class="gallery">
-            ${this.generateCards(listOfImageUrls)}
+            ${this.renderCards()}
         </div>
         `;
     }
